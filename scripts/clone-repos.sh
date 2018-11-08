a80a4082d8900b78267827e6b57632f2cbd08ae2
Daniel Martins Antunes <danoan2008@gmail.com>
2018-11-08 19:54:27 -0200

    Fix clone-repos script.

--- scripts/clone-repos.sh
+++ scripts/clone-repos.sh
@@ -9,21 +9,21 @@ rm $INSTALL_PATH -r -f
 
 DIPACUS_DIR=$EXT_PROJECT_DIR/DIPaCUS
 mkdir $DIPACUS_DIR -p
-git clone --single-branch -b interpixels https://github.com/danoan/DIPaCUS $DIPACUS_DIR
+git clone https://github.com/danoan/DIPaCUS $DIPACUS_DIR
 
 
 #--------------GEOC----------------
 
 GEOC_DIR=$EXT_PROJECT_DIR/geoc
 mkdir $GEOC_DIR -p
-git clone --single-branch -b interpixels https://github.com/danoan/GEOC $GEOC_DIR
+git clone https://github.com/danoan/GEOC $GEOC_DIR
 
 
 #--------------SCaBOliC----------------
 
 SCABOLIC_DIR=$EXT_PROJECT_DIR/SCaBOliC
 mkdir $SCABOLIC_DIR -p
-git clone --single-branch -b interpixels https://github.com/danoan/SCaBOliC $SCABOLIC_DIR
+git clone interpixels https://github.com/danoan/SCaBOliC $SCABOLIC_DIR
 
 
 
@@ -31,4 +31,4 @@ git clone --single-branch -b interpixels https://github.com/danoan/SCaBOliC $SCA
 
 BINOCS_DIR=$EXT_PROJECT_DIR/BinOCS
 mkdir $BINOCS_DIR -p
-git clone --single-branch -b interpixels https://github.com/danoan/BinOCS $BINOCS_DIR
+git clone interpixels https://github.com/danoan/BinOCS $BINOCS_DIR
