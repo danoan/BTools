6732cdb10f6ec2d85c49e686fc0b2b46c5860659
Daniel <danoan2008@gmail.com>
2019-03-29 15:24:18 +0100

    Input/Output changes for fromSeed application

--- Applications/fromSeed/src/FromSeedControl.cpp
+++ Applications/fromSeed/src/FromSeedControl.cpp
@@ -51,11 +51,28 @@ FromSeedControl::FromSeedControl(OptOutput& output,
                                          solution.outputDS); */
 
     output.IIElasticaValue =-1;
-    
+
     BTools::Utils::setHighlightedBorder(result.foreground,BTools::Utils::ColorType(255,255,0));
     BTools::Utils::setHighlightedBorder(bcaOutput.imgOutput,BTools::Utils::ColorType(255,255,0));
 
-    cv::imwrite(outputFolder + "/gc-seg.bmp",result.foreground);
-    cv::imwrite(outputFolder +"/corrected-seg.bmp",bcaOutput.imgOutput);
-    
+
+    std::string graphCutSegFilepath = outputFolder + "/gc-seg.png";
+    std::string correctedSegFilepath = outputFolder +"/corrected-seg.png";
+    std::string maskBoundaryFilepath = outputFolder +"/mask-boundary.png";
+
+
+
+    const DigitalSet& boundaryMaskDs = bcaOutput.energySolution.outputDS;
+    Point dims = boundaryMaskDs.domain().upperBound() - boundaryMaskDs.domain().lowerBound() + Point(1,1);
+    cv::Mat maskBoundaryImgGS = cv::Mat::zeros( dims(1),dims(0) ,CV_8UC1);
+    DIPaCUS::Representation::digitalSetToCVMat(maskBoundaryImgGS,boundaryMaskDs);
+
+    cv::Mat maskBoundaryImgColor( maskBoundaryImgGS.size(),CV_8UC3);
+    cv::cvtColor(maskBoundaryImgGS,maskBoundaryImgColor,cv::COLOR_GRAY2RGB);
+
+    BTools::Utils::setHighlightedBorder(maskBoundaryImgColor,cv::Vec3b(255,255,0));
+
+    cv::imwrite(graphCutSegFilepath,result.foreground);
+    cv::imwrite(correctedSegFilepath,bcaOutput.imgOutput);
+    cv::imwrite(maskBoundaryFilepath,maskBoundaryImgColor);
 }
