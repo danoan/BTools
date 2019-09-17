4001240c1606505182304cc2dd90415df4e8e07f
Daniel Antunes <dantu@lama-e47.univ-savoie.fr>
2019-09-17 15:22:43 +0200

    Dependencies cmake parameters

--- ext-projects/ExtProjects.cmake
+++ ext-projects/ExtProjects.cmake
@@ -1,8 +1,5 @@
 include(ExternalProject)
 
-set(EXTPROJECTS_SOURCE_DIR ${CMAKE_SOURCE_DIR}/ext-projects/source)
-set(EXTPROJECTS_BUILD_DIR ${CMAKE_SOURCE_DIR}/ext-projects/build)
-
 include(ext-projects/cmake-files/DIPaCUS.cmake)
 include(ext-projects/cmake-files/SCaBOliC.cmake)
 include(ext-projects/cmake-files/GEOC.cmake)
