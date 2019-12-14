884d597f89fd6ff84a76d014f89eccae28585b19
danoan <danoan2008@gmail.com>
2019-12-14 17:50:22 +0100

    Fix paths

--- jmiv-figures/fig10/jmiv-fig10.sh
+++ jmiv-figures/fig10/jmiv-fig10.sh
@@ -9,12 +9,12 @@ mkdir -p ${OUTPUT_FOLDER}
 
 BTOOLS_BIN=$(realpath $1)
 
-FIG7_APP=${BTOOLS_BIN}/jmiv-fig7
+FIG10_APP=${BTOOLS_BIN}/jmiv-fig10
 FLOW_APP=${BTOOLS_BIN}/shape-flow
 SUMMARY_APP=${BTOOLS_BIN}/summary-flow
 
 
-$FIG7_APP 20 80 15 ${DATA_FOLDER}/square-holes.pgm
+$FIG10_APP 20 80 15 ${DATA_FOLDER}/square-holes.pgm
 $FLOW_APP -i100 -r6 -p double -q1.0 -t0 -g0 -l-1 -S${DATA_FOLDER}/square-holes.pgm ${DATA_FOLDER}/square-holes-flow
 $SUMMARY_APP ${DATA_FOLDER}/square-holes-flow ${OUTPUT_FOLDER}/summary-square-holes.eps 10
 
