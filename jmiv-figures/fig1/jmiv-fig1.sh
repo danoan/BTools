f2e47cdba3831ffd1d971679b8cc80b7deaa87dc
danoan <danoan2008@gmail.com>
2020-01-23 11:24:36 +0100

    Library versioning

--- jmiv-figures/fig1/jmiv-fig1.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-FIG1_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-BTOOLS_BIN=$(realpath $1)
-
-FIG1_APP=${BTOOLS_BIN}/jmiv-fig1
-
-OUTPUT_FOLDER=${FIG1_ROOT}/output
-$FIG1_APP $OUTPUT_FOLDER
