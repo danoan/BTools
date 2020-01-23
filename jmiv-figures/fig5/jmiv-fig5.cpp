b66b29f7eb0787305371d2444848d3c1ddf88772
danoan <danoan2008@gmail.com>
2020-01-23 13:04:16 +0100

    jmiv-fig5 fix

--- jmiv-figures/fig5/jmiv-fig5.cpp
+++ jmiv-figures/fig5/jmiv-fig5.cpp
@@ -7,64 +7,101 @@
 
 #include <DIPaCUS/base/Shapes.h>
 #include <DIPaCUS/base/Representation.h>
-
 #include <DIPaCUS/components/SetOperations.h>
-
 #include <DIPaCUS/derivates/Misc.h>
+
+#include <SCaBOliC/Energy/ISQ/ISQEnergy.h>
+#include <SCaBOliC/Energy/ISQ/InputData.h>
+#include <SCaBOliC/Core/ODRPixels/ODRPixels.h>
+#include <SCaBOliC/Core/model/ODRModel.h>
 
 using namespace DGtal::Z2i;
 
-typedef DGtal::DistanceTransformation<DGtal::Z2i::Space, DigitalSet, DGtal::Z2i::L2Metric> DTL2;
-typedef DIPaCUS::Representation::Image2D Image2D;
+using namespace SCaBOliC::Core;
+using namespace SCaBOliC::Energy;
+using namespace SCaBOliC::Optimization;
 
-DTL2 interiorDistanceTransform(const Domain& domain, const DigitalSet& original)
+class MockDistribution:public SCaBOliC::Energy::ISQ::IProbabilityDistribution<double>
 {
-    return DTL2(domain, original, DGtal::Z2i::l2Metric);
-}
+public:
 
-DTL2 exteriorDistanceTransform(const Domain& domain, const DigitalSet& original)
-{
-    DigitalSet d(domain);
-    d.assignFromComplement(original);
-
-    return DTL2(domain, d, DGtal::Z2i::l2Metric);
-}
-
-DigitalSet level(const DTL2& dtL2, int lessThan, int greaterThan)
-{
-    DigitalSet d(dtL2.domain());
-    for(auto it=dtL2.domain().begin();it!=dtL2.domain().end();++it)
+    MockDistribution()
     {
-        if(dtL2(*it)<=lessThan && dtL2(*it)>greaterThan) d.insert(*it);
+        srand(time(NULL));
     }
 
-    return d;
+    double operator()(Index row, Index col) const
+    {
+        return 1.0/( ( rand()%20 )+1.1 );
+    }
+
+};
+
+std::pair<ODRPixels,ODRModel> createODRPair(const DigitalSet& shape)
+{
+    double radius=7;
+    double gridStep=1.0;
+    double levels=0;
+
+    ODRPixels odrPixels(radius,gridStep,levels,ODRModel::LD_CloserFromCenter,ODRPixels::NeighborhoodType::FourNeighborhood);
+    ODRModel odr = odrPixels.createODR(ODRPixels::OptimizationMode::OM_CorrectConvexities,
+                                       ODRPixels::ApplicationMode::AM_OptimizationBoundary,
+                                       shape);
+
+    return std::make_pair(odrPixels,odr);
 }
 
-int main(int argc, char* argv[])
+DigitalSet curveEvolution(const DigitalSet& shape)
 {
-    DigitalSet _shape = DIPaCUS::Shapes::square(1.0,0,0,20);
-    DigitalSet shape = DIPaCUS::Transform::bottomLeftBoundingBoxAtOrigin(_shape);
+    auto odrPair = createODRPair(shape);
+    const ODRPixels& odrPixels = odrPair.first;
+    const ODRModel& odr = odrPair.second;
 
-    std::string flowResultImage = argv[1];
-    std::string outputFolder = argv[2];
 
-    boost::filesystem::create_directories(outputFolder);
+    Point size = shape.domain().upperBound()-shape.domain().lowerBound() + Point(1,1);
+    cv::Mat mockImg( size[1],size[0],CV_8UC3);
 
+    MockDistribution mockFgDistr,mockBgDistr;
+
+    ISQEnergy::InputData isqInput(odr,mockImg,mockFgDistr,mockBgDistr,false,false,0,1.0,0);
+    ISQEnergy isq(isqInput,odrPixels.handle());
+
+    Solution solution(shape.domain(),isq.numVars());
+    isq.solve<QPBOImproveSolver>(solution);
+
+    Solution::LabelsVector notInverted(isq.numVars());
+    notInverted.setZero();
+    for(int i=0;i<isq.numVars();++i)
+    {
+        if(solution.labelsVector.coeff(i)==0) notInverted.coeffRef(i) = 1;
+    }
+
+    DigitalSet dsOut(shape.domain());
+    DigitalSet dsIn = odr.trustFRG;
+    odrPixels.handle()->solutionSet(dsOut,
+                                    dsIn,
+                                    odr,
+                                    ODRPixels::OptimizationMode::OM_CorrectConvexities,
+                                    notInverted.data(),
+                                    isq.vm().pim);
+
+    return dsOut;
+
+}
+
+void beforeOpt(const DigitalSet& shape, const std::string& outputFilepath)
+{
+    auto odrPair = createODRPair(shape);
+    const ODRPixels& odrPixels = odrPair.first;
+    const ODRModel& odr = odrPair.second;
+
+    const DigitalSet& frgRegion = odr.trustFRG;
+    const DigitalSet& optRegion = odr.optRegion;
 
     Point lb,ub;
     shape.computeBoundingBox(lb,ub);
 
     DigitalSet ball = DIPaCUS::Shapes::ball(1.0,ub[0],ub[1],10);
-
-    DTL2 innDT = interiorDistanceTransform(shape.domain(),shape);
-    DTL2 outDT = exteriorDistanceTransform(shape.domain(),shape);
-
-    DigitalSet optRegion = level(outDT,1,0);
-    //optRegion += level(innDT,1,0);
-
-    DigitalSet frgRegion(shape.domain());
-    DIPaCUS::SetOperations::setDifference(frgRegion,shape,optRegion);
 
     DigitalSet frgIntersection(shape.domain());
     DIPaCUS::SetOperations::setIntersection(frgIntersection,frgRegion,ball);
@@ -76,51 +113,81 @@ int main(int argc, char* argv[])
     board << DGtal::SetMode(shape.className(),"Paving");
 
     board << DGtal::CustomStyle( shape.className() + "/Paving",new DGtal::CustomColors(DGtal::Color::Silver, DGtal::Color::Blue))
-    << shape;
-
-    board.saveEPS( (outputFolder + "/shape.eps").c_str() );
+          << shape;
 
     board
-    << DGtal::CustomStyle( frgRegion.className() + "/Paving",new DGtal::CustomColors(DGtal::Color::Silver, DGtal::Color::Blue))
-    << frgRegion
-    << DGtal::CustomStyle( optRegion.className() + "/Paving",new DGtal::CustomColors(DGtal::Color::Silver, DGtal::Color::Yellow) )
-    << optRegion;
+            << DGtal::CustomStyle( frgRegion.className() + "/Paving",new DGtal::CustomColors(DGtal::Color::Silver, DGtal::Color::Blue))
+            << frgRegion
+            << DGtal::CustomStyle( optRegion.className() + "/Paving",new DGtal::CustomColors(DGtal::Color::Silver, DGtal::Color::Yellow) )
+            << optRegion
+            << DGtal::CustomStyle( ball.className() + "/Paving",new DGtal::CustomColors(DGtal::Color::Silver, DGtal::Color::Gray) )
+            << ball
+            << DGtal::CustomStyle( frgIntersection.className() + "/Paving",new DGtal::CustomColors(DGtal::Color::Silver, DGtal::Color::Navy) )
+            << frgIntersection
+            << DGtal::CustomStyle( optIntersection.className() + "/Paving",new DGtal::CustomColors(DGtal::Color::Silver, DGtal::Color(255,128,0)) )
+            << optIntersection;
 
-    board.saveEPS( (outputFolder + "/shape-opt.eps").c_str() );
+    board.saveEPS( outputFilepath.c_str() );
+}
 
-    board
-    << DGtal::CustomStyle( ball.className() + "/Paving",new DGtal::CustomColors(DGtal::Color::Silver, DGtal::Color::Gray) )
-    << ball
-    << DGtal::CustomStyle( frgIntersection.className() + "/Paving",new DGtal::CustomColors(DGtal::Color::Silver, DGtal::Color::Navy) )
-    << frgIntersection
-    << DGtal::CustomStyle( optIntersection.className() + "/Paving",new DGtal::CustomColors(DGtal::Color::Silver, DGtal::Color(255,128,0)) )
-    << optIntersection;
+void afterOpt(const DigitalSet& shape,const std::string& outputFilepath)
+{
+    auto odrPair = createODRPair(shape);
+    const ODRPixels& odrPixels = odrPair.first;
+    const ODRModel& odr = odrPair.second;
 
-    board.saveEPS(  (outputFolder + "/shape-opt-ball.eps").c_str());
+    const DigitalSet& frgRegion = odr.trustFRG;
+    const DigitalSet& optRegion = odr.optRegion;
+
+    Point lb,ub;
+    shape.computeBoundingBox(lb,ub);
+
+    DGtal::Board2D board;
+    DigitalSet ball = DIPaCUS::Shapes::ball(1.0,ub[0],ub[1],10);
+
+
+    DigitalSet frgIntersection(shape.domain());
+    DIPaCUS::SetOperations::setIntersection(frgIntersection,frgRegion,ball);
+
+    DigitalSet optIntersection(shape.domain());
+    DIPaCUS::SetOperations::setIntersection(optIntersection,optRegion,ball);
+
+    board << DGtal::SetMode(shape.className(),"Paving");
 
     {
-        Image2D img = DGtal::GenericReader<Image2D>::import(flowResultImage);
-        DigitalSet it(img.domain());
-        DIPaCUS::Representation::imageAsDigitalSet(it,img);
+        DigitalSet optDS = curveEvolution(shape);
 
         Point ub,lb;
-        it.computeBoundingBox(lb,ub);
+        optDS.computeBoundingBox(lb,ub);
         DigitalSet ball = DIPaCUS::Shapes::ball(1.0,ub[0]-1,ub[1]-1,10);
 
-        board.clear();
-
-        DigitalSet ballIntersection(it.domain());
-        DIPaCUS::SetOperations::setIntersection(ballIntersection,it,ball);
+        DigitalSet ballIntersection(optDS.domain());
+        DIPaCUS::SetOperations::setIntersection(ballIntersection,optDS,ball);
 
         board << DGtal::CustomStyle( frgRegion.className() + "/Paving",new DGtal::CustomColors(DGtal::Color::Silver, DGtal::Color::Blue))
-              << it
+              << optDS
               << DGtal::CustomStyle( ball.className() + "/Paving",new DGtal::CustomColors(DGtal::Color::Silver, DGtal::Color::Gray) )
               << ball
-              << DGtal::CustomStyle( frgIntersection.className() + "/Paving",new DGtal::CustomColors(DGtal::Color::Silver, DGtal::Color::Navy) )
+              << DGtal::CustomStyle( ballIntersection.className() + "/Paving",new DGtal::CustomColors(DGtal::Color::Silver, DGtal::Color::Navy) )
               << ballIntersection;
 
-        board.saveEPS( (outputFolder + "/shape-opt-ball-after.eps").c_str() );
+        board.saveEPS( outputFilepath.c_str() );
     }
+
+}
+
+int main(int argc, char* argv[])
+{
+    DigitalSet _shape = DIPaCUS::Shapes::square(1.0,0,0,20);
+    DigitalSet shape = DIPaCUS::Transform::bottomLeftBoundingBoxAtOrigin(_shape);
+
+    std::string outputFolder = argv[1];
+
+    boost::filesystem::create_directories(outputFolder);
+
+    beforeOpt(shape,outputFolder+"/before-opt.eps");
+    afterOpt(shape,outputFolder + "/shape-opt-ball-after.eps");
+
 
 
     return 0;
