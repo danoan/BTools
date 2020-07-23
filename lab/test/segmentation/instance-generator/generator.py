77a47af44d77b819d56f8faec8f88a53dbe0f7d3
Daniel <danoan2008@gmail.com>
2020-07-23 15:34:25 +0200

    Separate test from experiments. Do not render html template in tests

--- lab/test/segmentation/instance-generator/generator.py
+++ lab/test/segmentation/instance-generator/generator.py
@@ -3,7 +3,6 @@ import subprocess,sys,os,time
 
 from param_combinator import *
 from config import *
-from template_render import *
 
 SCRIPT_FOLDER="set in read  input"
 BINARY_FOLDER="set in read  input"
@@ -68,8 +67,6 @@ def main():
         if valid_combination(c):
             boundary_correction(c)
             export_seed_mask(c)
-
-    render_template("boundary-correction",CONFIG_LIST,"%s/%s" % (os.path.dirname(SCRIPT_FOLDER),"output"))
 
 
 if __name__=='__main__':
---
+++ lab/test/shape-flow/test-shape-flow.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+SCRIPT_FOLDER=$( cd $(dirname ${BASH_SOURCE[0]} ) > /dev/null 2>&1 && pwd )
+BTOOLS_BIN=$(realpath $1)
+OUTPUT_FOLDER=$(realpath $2)
+
+python3 ${SCRIPT_FOLDER}/instance-generator/generator.py ${BTOOLS_BIN} ${OUTPUT_FOLDER}
