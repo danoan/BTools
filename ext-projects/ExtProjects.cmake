151cc4d47c92671afdd70859eb5723135717d8f9
danoan <danoan2008@gmail.com>
2020-09-10 09:51:12 +0200

    Include SELECTED_FEATURES parameter in CMake script

--- ext-projects/ExtProjects.cmake
+++ ext-projects/ExtProjects.cmake
@@ -1,5 +1,10 @@
 include(ExternalProject)
 
-include(ext-projects/cmake-files/DIPaCUS.cmake)
-include(ext-projects/cmake-files/SCaBOliC.cmake)
-include(ext-projects/cmake-files/GEOC.cmake)
+if(${SELECTED_FEATURES} STREQUAL "ALL")
+    include(ext-projects/cmake-files/DIPaCUS.cmake)
+    include(ext-projects/cmake-files/GEOC.cmake)
+    include(ext-projects/cmake-files/SCaBOliC.cmake)
+elseif(${SELECTED_FEATURES} STREQUAL "SEED_SELECTION")
+    include(ext-projects/cmake-files/DIPaCUS.cmake)
+    include(ext-projects/cmake-files/GEOC.cmake)
+endif()
